c2f702f5d6a5f818068b34d789f7cb6cbecbc1b9
2langk <leelk980@gmail.com>
2024-03-12 23:28:16 +0900

    feat: setup nestia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstrap();
+bootstrap().then(() =>
+  console.log(
+    `[${process.env['NODE_ENV']?.toLocaleUpperCase()}] Server is running on 3000`,
+  ),
+);
